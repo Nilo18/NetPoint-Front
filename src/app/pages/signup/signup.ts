dde7e56f34517ad2e84e5510276471e407efa56e
Nilo18 <nikalongurashvili@gmail.com>
2026-04-01 23:44:38 +0400

    Added form validation to signup

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -10,6 +10,7 @@ import { FormValidatorService } from '../../services/form-validator-service';
 })
 export class Signup {
   signupForm!: FormGroup
+  signupFormStageTwo!: FormGroup
   showNextStep: boolean = false
   private formValidator = inject(FormValidatorService)
   private fb = inject(FormBuilder) 
@@ -17,14 +18,59 @@ export class Signup {
   ngOnInit() {
     this.signupForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, /*Validators.minLength(8)*/]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
       confirm_password: ['', [Validators.required]],
+    }, { validators: this.formValidator.passwordMatchValidator() })
+
+    this.signupFormStageTwo = this.fb.group({
       name: ['', [Validators.required]],
       industry: ['', [Validators.required]]
-    }, { validators: this.formValidator.passwordMatchValidator() })
+    })
+    console.log(this.email)
   }
 
   setShowNextStep(val: boolean) {
     this.showNextStep = val
+  }
+
+  get email() {
+    return this.signupForm.value.email
+  }
+
+  getError(field: string, form: FormGroup) {
+    return this.formValidator.getError(field, form)
+  }
+
+  onSubmit() {
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched()
+      console.log('The form is invalid.')
+      console.log(this.formValidator.getError('confirm_password', this.signupFormStageTwo))
+      return this.formValidator.getError('confirm_password', this.signupFormStageTwo)
+    }
+
+    if (!this.showNextStep) {
+      this.setShowNextStep(true)
+      console.log('Showing next step...')
+      // return
+    } 
+
+    return
+    // else {
+    //   console.log(this.signupForm.value)
+    //   // return
+    // }
+  }
+
+  onFinalSubmit() {
+    if (this.signupFormStageTwo.invalid) {
+      this.signupFormStageTwo.markAllAsTouched()
+      console.log('The form is invalid.')
+      return
+    }
+
+    if (this.showNextStep) {
+      console.log(this.signupForm.value)
+    }
   }
 }
--- src/app/services/form-validator-service.ts
+++ src/app/services/form-validator-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,41 @@ export class FormValidatorService {
       const password = formGroup.get('password')?.value
       const confirmPassword = formGroup.get('confirm_password')?.value
 
+      console.log(`Compared ${password} to ${confirmPassword}`)
+
       return password === confirmPassword ? null : { passwordMismatch: true }
     }
+  }
+
+  formatLabel(field: string): string {
+    return field
+      .replace(/_/g, ' ')                        
+      .replace(/^\w/, c => c.toUpperCase());    
+  }
+
+  getError(field: string, form: FormGroup) {
+    const control = form.get(field)
+    const label = this.formatLabel(field)
+    // console.log(label)
+
+    if (!control || !control.touched) return ''
+
+    if (control.hasError('required')) { 
+      // field.charAt(0)
+      return `${label} is required.`
+    }
+    if (control.hasError('email')) return 'Invalid email address.'
+    if (control.hasError('minlength')) {
+      const min = control.getError('minlength').requiredLength
+      console.log(control.getError('minlength'))
+      console.log('value of min is: ', min)
+      return `${label} must be at least ${min} characters long`
+    }
+
+    if (field === 'confirm_password' && form.hasError('passwordMismatch')) {
+      return 'Passwords do not match';
+    }
+
+    return ''
   }
 }
