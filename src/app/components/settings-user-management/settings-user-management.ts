9061f56243753fffd0ca7f286d3d477873faa283
Nilo18 <nikalongurashvili@gmail.com>
2026-04-29 22:52:20 +0400

    feature: connect settings user management to backend

--- src/app/components/settings-user-management/settings-user-management.ts
+++ src/app/components/settings-user-management/settings-user-management.ts
@@ -1,18 +1,30 @@
-import { Component, inject } from '@angular/core';
-import { SettingsPageService } from '../../services/settings-page-service';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { SettingsPageService, User } from '../../services/settings-page-service';
+import { jwtDecode } from 'jwt-decode';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-settings-user-management',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './settings-user-management.html',
   styleUrl: './settings-user-management.scss',
 })
 export class SettingsUserManagement {
   private settingsService = inject(SettingsPageService)
-  accessToken!: string
+  private cdr = inject(ChangeDetectorRef)
+  decodedToken!: any
+  userList: User[] = []
 
   async ngOnInit() {
     const token = localStorage.getItem('net_token') 
-    // await this.settingsService.getUserlist()
+
+    if (token) {
+      this.decodedToken = jwtDecode(token)
+      console.log(this.decodedToken)
+      const res = await this.settingsService.getUserlist(Number(this.decodedToken.companyId), 0, 10)
+      this.userList = res.userList
+      this.cdr.detectChanges()
+      console.log('The local userList is: ', this.userList)
+    }
   }
 }
