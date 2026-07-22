17c3c31f0747f17e60c1688637fa33c21ecffd04
Nilo18 <nikalongurashvili@gmail.com>
2026-07-22 13:18:19 +0400

    feature: add audit log stat layout

--- src/app/components/audit-log-page-components/audit-log-stats/audit-log-stats.ts
+++ src/app/components/audit-log-page-components/audit-log-stats/audit-log-stats.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface AuditLogStat {
+  statName: string,
+  statNumber: number
+}
+
 @Component({
   selector: 'app-audit-log-stats',
   imports: [],
@@ -7,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrl: './audit-log-stats.scss',
 })
 export class AuditLogStats {
-
+  stats: AuditLogStat[] = [
+    {
+      statName: 'TOTAL EVENTS',
+      statNumber: 20
+    },
+    {
+      statName: 'SALES',
+      statNumber: 6
+    },
+    {
+      statName: 'PRODUCT CHANGES',
+      statNumber: 3
+    },
+    {
+      statName: 'TEAM CHANGES',
+      statNumber: 3
+    },
+    {
+      statName: 'PAYMENT CHANGES',
+      statNumber: 4
+    },
+    {
+      statName: 'ACCOUNT CHANGES',
+      statNumber: 4
+    },
+  ]
 }
