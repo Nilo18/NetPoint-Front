d2df41d6a56d68e4fc6a1ec798f1f12d1fdb435d
Nilo18 <nikalongurashvili@gmail.com>
2026-08-17 12:56:43 +0400

    feature: add backend production url

--- src/app/services/backend-url-holder-service.ts
+++ src/app/services/backend-url-holder-service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT, inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BackendUrlHolderService {
+  private document = inject(DOCUMENT)
   private baseUrl = 'http://localhost:8080'
+  private apiUrl = 'https://netpoint-back.onrender.com'
+
+  isLocalHost() {
+    return this.document.location.hostname === 'localhost' || this.document.location.hostname === '127.0.0.1'
+  }
 
   getBaseUrl() {
-    return this.baseUrl
+    return this.isLocalHost()? this.baseUrl : this.apiUrl
   }
 }
